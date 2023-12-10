2203b5be6d560ed3ae5b6ca97fe473e1d01d9bb9
Michal Nowacki <nowacki.michal77@gmail.com>
2023-12-10 19:05:15 +0100

    Traffic at instance level

---
+++ 008-aws-traffic/main.tf
@@ -0,0 +1,88 @@
+resource "aws_vpc" "vnet_nebo" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vnet-nebo"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vnet_nebo.id
+}
+
+resource "aws_route_table" "my_route" {
+  vpc_id = aws_vpc.vnet_nebo.id
+}
+
+
+resource "aws_route" "outbound_route" {
+  route_table_id = aws_route_table.my_route.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id = aws_subnet.vnet_public.id
+  route_table_id = aws_route_table.my_route.id
+}
+
+resource "aws_subnet" "vnet_public" {
+  vpc_id                  = aws_vpc.vnet_nebo.id
+  cidr_block             = "10.0.0.0/17"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "snet-public"
+  }
+}
+
+# Security Group for VM2 (snet-public)
+resource "aws_security_group" "vm1_sg" {
+  vpc_id = aws_vpc.vnet_nebo.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]   
+  }
+}
+
+
+resource "aws_instance" "vm1" {
+  ami             = "ami-06dd92ecc74fdfb36"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.vnet_public.id
+  key_name        = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.vm1_sg.id]
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update
+              apt install -y nginx
+              echo '<html><body><h1>Configured firewall</h1><p>Configure traffic control at the Instance Level</p></body></html>' > /var/www/html/index.nginx-debian.html 
+              sudo systemctl restart nginx
+              EOF
+  tags = {
+    Name = "VM1"
+  }
+
+}
+
+# Output Public IP of VM2 for accessibility
+output "vm1_public_ip" {
+  value = aws_instance.vm1.public_ip
+}
+
+
+# Key pair
+resource "aws_key_pair" "my_key" {
+  key_name = "my_key"
+  public_key = file("~/.ssh/devops.pub")
+} 
